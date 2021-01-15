50dfde74f0afb2ced5ef0f28ce6d514aec356250
Rafail Dardagiannopoulos <77059808+Rafail1992@users.noreply.github.com>
2021-01-15 23:58:53 +0200

    Version V1.3

--- pages/signup_request_page.php
+++ pages/signup_request_page.php
@@ -30,6 +30,57 @@ if($password != $password_confirm){
     exit;
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Check duplication for email
+//Build the request 
+$query_1 = "SELECT userID  FROM Names where email = ?";
+//Prepare the connection to send the query
+$stmt_1 = $conn -> prepare($query_1);
+//Add the parameters
+$stmt_1 -> bind_param('s',$username);
+//Execute the query
+$stmt_1 -> execute();
+//Store the data after the execution of the query
+$stmt_1 -> store_result();
+//Restore the results and create a variable with the value
+$stmt_1 -> bind_result($checker_1);
+//Use the values with the fetch funcion
+$stmt_1 -> fetch();
+// check if the email exist
+if($checker_1){
+    //if it exist go back to the index file
+    header("location:../index.php");
+    $_SESSION['newscript']="<script>$('#pop_up_help').show();var exercise = document.getElementById('main_page_menu');var warning = document.getElementById('warning_msg');function add_warning(){warning.innerHTML = 'This email already exists!';warning.scrollIntoView();}window.addEventListener('load',add_warning,false);</script>";
+    exit;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Check duplication for username
+//Build the request 
+$query_2 = "SELECT userID  FROM Names where user = ?";
+//Prepare the connection to send the query
+$stmt_2 = $conn -> prepare($query_2);
+//Add the parameters
+$stmt_2 -> bind_param('s',$email);
+//Execute the query
+$stmt_2 -> execute();
+//Store the data after the execution of the query
+$stmt_2 -> store_result();
+//Restore the results and create a variable with the value
+$stmt_2 -> bind_result($checker_2);
+//Use the values with the fetch funcion
+$stmt_2 -> fetch();
+// check if the email exist
+if($checker_2){
+    //if it exist go back to the index file
+    header("location:../index.php");
+    $_SESSION['newscript']="<script>$('#pop_up_help').show();var exercise = document.getElementById('main_page_menu');var warning = document.getElementById('warning_msg');function add_warning(){warning.innerHTML = 'This email already exists!';warning.scrollIntoView();}window.addEventListener('load',add_warning,false);</script>";
+    exit;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 $conn = new mysqli("sql101.epizy.com", "epiz_27368706", "TI4qfAS8Hsege4M", "epiz_27368706_002");
 
 if(mysqli_errno()){
