cbc38759f5fe7c0edce5b8fbddc677de054e2f5b
Rafail Dardagiannopoulos <77059808+Rafail1992@users.noreply.github.com>
2021-01-16 16:00:25 +0200

    Version V1.6
    Structure update

--- js/carousel_img_size_script.js
+++ js/carousel_img_size_script.js
@@ -1,11 +1,21 @@
 // JavaScript Document
+//Execute the function, when the window load
 $(window).on('load',function(){
+	
+//Store the width of the window
 	var width = $(window).innerWidth();
+	
+//Change the carousel image size
 	$(".img_carousel").css("width",width);
 });
 
+//Execute the function, when the window resized
 $(window).resize(function(){
+	
+//Store the width of the window
 	var width = $(window).innerWidth();
+
+//Change the carousel image size
 	$(".img_carousel").css("width",width);
 });
 
